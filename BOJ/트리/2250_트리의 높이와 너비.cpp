79afa40376947685f1c4bfcd99bd1ce3c5e1555a
최현석 <43382559+hschoi1104@users.noreply.github.com>
2019-09-19 20:40:55 +0900

    Update 2250_트리의 높이와 너비.cpp

--- BOJ/트리/2250_트리의 높이와 너비.cpp
+++ BOJ/트리/2250_트리의 높이와 너비.cpp
@@ -1,63 +1,45 @@
-#include <stdio.h>
+#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <limits.h>
 using namespace std;
 typedef pair<int, int> p;
-vector<vector<int>>v;
-int INF = 999999999;
-int indegree[10001];
-int order[10001];
-int level[10001];
-int ans[10001];
-int t = 1;
-p res[10001];
-void travel(int cur, int l) {
-	int left = v[cur][0];
-	int right = v[cur][1];
-	if (left != -1) {
-		travel(left, l + 1);
-	}
-	order[cur] = t++;
-	level[cur] = l;
-	if (right != -1) {
-		travel(right, l + 1);
-	}
+vector<vector<int>>g;
+int arr[10010];
+p level[10002];
+int indeg[10002];
+int cnt = 0, ans = 0, root = 0;
+void dfs(int cur, int lv) {
+	if (g[cur][0] != -1) dfs(g[cur][0], lv + 1);
+	arr[cur] = ++cnt;
+	if (g[cur][1] != -1) dfs(g[cur][1], lv + 1);
+	level[lv] = { min(level[lv].first, arr[cur]), max(level[lv].second, arr[cur]) };
 	return;
 }
 int main() {
-	int n, a, b, c, root = 0;
-	scanf("%d", &n);
-	v.resize(n + 1);
-	//input
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int n; cin >> n;
+	g.resize(n + 1);
+	for (int i = 0; i < 10002; i++) { level[i] = { INT_MAX,0 }; }
 	for (int i = 0; i < n; i++) {
-		scanf("%d %d %d", &a, &b, &c);
-		v[a].push_back(b); v[a].push_back(c);
-		indegree[b] += 1; indegree[c] += 1;
-	}
-	//find root
-	for (int i = 1; i <= n; i++) {
-		if (indegree[i] == 0) {
-			root = i;
-			break;
-		}
-	}
-	//travle
-	travel(root, 1);
-
-	//chk level-width
-	int maxwidth = 0;
-	int ans = INF;
-	for (int i = 0; i <= n; i++) res[i] = { INF,0 };
-	for (int i = 1; i <= n; i++) {
-		res[level[i]].first = min(res[level[i]].first, order[i]);
-		res[level[i]].second = max(res[level[i]].second, order[i]);
-		if (res[level[i]].first != INF)maxwidth = max(maxwidth, res[level[i]].second - res[level[i]].first + 1);
+		int a, b, c; cin >> a >> b >> c;
+		indeg[b] += 1; indeg[c] += 1;
+		g[a].push_back(b); g[a].push_back(c);
 	}
 
-	//find ans
+	for (int i = 1; i <= n; i++)
+		if (indeg[i] == 0) { root = i; break; }
+
+	dfs(root, 1);
+
 	for (int i = 1; i <= n; i++) {
-		if (res[i].first != INF && maxwidth == res[i].second - res[i].first + 1)ans = min(ans, i);
+		if (level[i].first == INT_MAX) break;
+		ans = max(ans, level[i].second - level[i].first + 1);
 	}
-	printf("%d %d", ans, maxwidth);
+
+	for (int i = 1; i <= n; i++) {
+		if (level[i].first == INT_MAX) break;
+		if (ans == level[i].second - level[i].first + 1) { cout << i << " " << ans; return 0; }
+	}
 	return 0;
 }
