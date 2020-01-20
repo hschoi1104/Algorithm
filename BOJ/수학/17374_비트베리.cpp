b349830eb9bf4c8d774d41e68324ef5540da0eca
최현석 <43382559+hschoi1104@users.noreply.github.com>
2020-01-20 16:48:21 +0900

    Update 17374_비트베리.cpp

--- BOJ/수학/17374_비트베리.cpp
+++ BOJ/수학/17374_비트베리.cpp
@@ -10,11 +10,11 @@ int main() {
 		if (q >= c) coin += d * (q / c);
 		int res = (p - coin) / (a + b);
 		int tmp = res;
-		if (p - a * tmp >= 0 && coin + b * tmp >= 0) ans = min(p - a * tmp, coin + b * tmp);
+		ans = min(p - a * tmp, coin + b * tmp);
 		tmp = res - 1;
-		if (p - a * tmp >= 0 && coin + b * tmp >= 0) ans = max(ans,min(p - a * tmp, coin + b * tmp));
+		ans = max(ans,min(p - a * tmp, coin + b * tmp));
 		tmp = res + 1;
-		if (p - a * tmp >= 0 && coin + b * tmp >= 0) ans = max(ans,min(p - a * tmp, coin + b * tmp));
+		ans = max(ans,min(p - a * tmp, coin + b * tmp));
 		cout << ans << "\n";
 	}
 	return 0;
