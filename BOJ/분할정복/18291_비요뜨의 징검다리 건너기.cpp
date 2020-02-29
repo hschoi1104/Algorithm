334f7dd908e1df1bcd2897d6eb5f8c37bb6a2b3d
최현석 <43382559+hschoi1104@users.noreply.github.com>
2020-03-01 05:49:25 +0900

    Update 18291_비요뜨의 징검다리 건너기.cpp

--- BOJ/분할정복/18291_비요뜨의 징검다리 건너기.cpp
+++ BOJ/분할정복/18291_비요뜨의 징검다리 건너기.cpp
@@ -3,12 +3,13 @@
 using namespace std;
 typedef long long ll;
 ll go(int n) {
-	if (n == 1|| n == 0) return 2;
+	if (n == 1 || n == 0) return 2;
 	ll tmp = go(n / 2);
 	if (n % 2) return (tmp * tmp * 2) % MOD;
 	return (tmp * tmp) % MOD;
 }
 int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	int t; cin >> t;
 	while (t--) {
 		ll n; cin >> n;
