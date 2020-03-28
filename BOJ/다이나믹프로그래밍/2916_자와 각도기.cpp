0825ecae66aa3dec7ee775e35a21a06d4446cd8d
최현석 <43382559+hschoi1104@users.noreply.github.com>
2020-03-28 20:28:39 +0900

    Create 2916_자와 각도기.cpp

---
+++ BOJ/다이나믹프로그래밍/2916_자와 각도기.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define FIO ios_base::sync_with_stdio(false), cin.tie(),cout.tie();
+using namespace std;
+int n, m, x;
+vector<int> a;
+int dp[13][361];
+bool ans[361];
+
+void go(int idx, int radi) {
+    ans[radi] = true;
+    if (idx == n) return;
+    int &ret = dp[idx][radi];
+    if (ret != -1) return;
+    ret = 1;
+    go(idx + 1, (radi + a[idx]) % 360);
+    go(idx, (radi + a[idx]) % 360);
+    go(idx + 1, (radi - a[idx] + 360) % 360);
+    go(idx + 1, radi);
+}
+
+int main() {
+    FIO;
+    memset(dp, -1, sizeof(dp));
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        a.push_back(x);
+    }
+    go(0, 0);
+    for (int i = 0; i < m; i++) {
+        cin >> x;
+        if (ans[x]) cout << "YES\n";
+        else cout << "NO\n";
+    }
+
+    return 0;
+}
