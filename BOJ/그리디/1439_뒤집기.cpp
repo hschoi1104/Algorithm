6fd0691e40ffa2e2d013888e853c912f61a68d4f
최현석 <43382559+hschoi1104@users.noreply.github.com>
2020-04-23 19:21:52 +0900

    Create 1439_뒤집기.cpp

---
+++ BOJ/그리디/1439_뒤집기.cpp
@@ -0,0 +1,19 @@
+﻿#include <bits/stdc++.h>
+
+#define FIO ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+using namespace std;
+
+int main() {
+	FIO;
+	string s; cin >> s;
+	int prv = 0,cnt=0;
+	for (int i = 0; i < s.length(); i++) {
+		if (i == 0) prv = s[i];
+		if (prv != s[i]) {
+			cnt += 1;
+			prv = s[i];
+		}
+	}
+	cout << (cnt + 1)/2;
+	return 0;
+}
