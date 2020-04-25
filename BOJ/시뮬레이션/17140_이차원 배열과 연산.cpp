6770415ea8c30f11a799f87597e594e0907fe95b
최현석 <43382559+hschoi1104@users.noreply.github.com>
2020-04-26 02:43:59 +0900

    Update 17140_이차원 배열과 연산.cpp

--- BOJ/시뮬레이션/17140_이차원 배열과 연산.cpp
+++ BOJ/시뮬레이션/17140_이차원 배열과 연산.cpp
@@ -73,3 +73,56 @@ int main() {
 	cout << solve();
 	return 0;
 }
+
+//#include <bits/stdc++.h>
+//
+//#define FIO ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+//#define IFOR(a,b) for(int i=a;i<b;i++)
+//#define JFOR(a,b) for(int j=a;j<b;j++)
+//using namespace std;
+//int r, c, k, a[110][110], mr, mc, chk[101];
+//struct node {
+//	int n, cnt;
+//	bool operator <(const node& tmp)const {
+//		if (cnt == tmp.cnt) return n < tmp.n;
+//		return cnt < tmp.cnt;
+//	}
+//};
+//vector<node>v;
+//
+//void P(int ty) {
+//	int tmp = 0, trg = (ty ? mr : mc), trg2 = (ty ? mc : mr);
+//	IFOR(0, trg) {
+//		IFOR(1, 101)chk[i] = 0;
+//		v.clear();
+//		JFOR(0, trg2) (ty ? chk[a[i][j]] : chk[a[j][i]]) += 1;
+//		JFOR(1, 101) if (chk[j]) v.push_back({ j,chk[j] });
+//		sort(v.begin(), v.end());
+//		JFOR(0, v.size()) {
+//			if (j == 50) break;
+//			(ty ? a[i][j * 2] : a[j * 2][i]) = v[j].n;
+//			(ty ? a[i][j * 2 + 1] : a[j * 2 + 1][i]) = v[j].cnt;
+//			tmp = max(tmp, j * 2 + 2);
+//		}
+//		JFOR(v.size() * 2, 100)(ty ? a[i][j] : a[j][i]) = 0;
+//	}
+//	(ty ? mc : mr) = tmp;
+//	return;
+//}
+//int main() {
+//	FIO;
+//	cin >> r >> c >> k;
+//	IFOR(0, 3)JFOR(0, 3) cin >> a[i][j];
+//	r -= 1; c -= 1;
+//	mr = mc = 3;
+//	int t = 101;
+//	while (t--) {
+//		if (a[r][c] == k) {
+//			cout << 100 - t;
+//			return 0;
+//		}
+//		(mr >= mc ? P(1) : P(0));
+//	}
+//	cout << -1;
+//	return 0;
+//}
