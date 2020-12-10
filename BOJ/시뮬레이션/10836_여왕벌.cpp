8accdcacae4523025f6e440b2de2f41101286a4b
hschoi1104 <hschoi1104@gmail.com>
2020-12-10 16:49:05 +0900

    Update 10836_여왕벌.cpp

--- BOJ/시뮬레이션/10836_여왕벌.cpp
+++ BOJ/시뮬레이션/10836_여왕벌.cpp
@@ -6,8 +6,6 @@
 //
 
 #include <iostream>
-#include <string>
-#include <queue>
 #define FIO  ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
 using namespace std;
 int M,N,tmp[1401],a,b,c;
@@ -18,15 +16,11 @@ int main(){
     
     for(int i=0;i<N;i++){
         cin>>a>>b>>c;
-        for(int j=a;j<a+b;j++) tmp[j]+=1;
-        for(int j=a+b;j<2*M-1;j++) tmp[j]+=2;
+        for(int j=a;j<2*M-1;j++) tmp[j] += (j<a+b ? 1:2);
     }
-  
+    
     for(int y=0;y<M;y++){
-        for(int x=0;x<M;x++){
-            if(x == 0) cout<<tmp[M-y-1]+1<<" ";
-            else cout<<tmp[M+x-1]+1<<" ";
-        }
+        for(int x=0;x<M;x++) cout<<(x==0 ? tmp[M-y-1] : tmp[M+x-1])+1<<" ";
         cout<<"\n";
     }
     return 0;
