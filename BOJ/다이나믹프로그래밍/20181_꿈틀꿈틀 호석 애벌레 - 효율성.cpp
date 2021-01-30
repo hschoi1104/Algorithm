c93e4abd84aef823dc28ff94f8d7956a1c0d82b2
hschoi1104 <hschoi1104@gmail.com>
2021-01-30 17:05:56 +0900

    Create 20181_꿈틀꿈틀 호석 애벌레 - 효율성.cpp

---
+++ BOJ/다이나믹프로그래밍/20181_꿈틀꿈틀 호석 애벌레 - 효율성.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  boj
+//
+//  Created by seok on 2021/01/28.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+ll n,k,sum,arr[100010],dp[100010];
+
+int main(){
+    ios::sync_with_stdio(false), cin.tie(0);
+    cin>>n>>k;
+    for(int i=1;i<=n;i++) cin>>arr[i];
+    for(int r=1,l=0;r<=n;r++){
+        sum+=arr[r];
+        dp[r]=dp[r-1];
+        while(sum>=k){
+            dp[r]=max(dp[r],dp[l]+sum-k);
+            sum-=arr[++l];
+        }
+    }
+    cout<<dp[n]<<"\n";
+    
+    return 0;
+}
